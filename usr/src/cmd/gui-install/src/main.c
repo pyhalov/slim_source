1450f8b968837dc1df3b300ebf99aa22dcdfcb56
Alexander Pyhalov <apyhalov@gmail.com>
2018-04-23 22:50:10 +0300

    9495 gui-install dumps core after PR#{39,40}

--- usr/src/cmd/gui-install/src/main.c
+++ usr/src/cmd/gui-install/src/main.c
@@ -545,9 +545,11 @@ main(int argc, char *argv[])
 	bindtextdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
 	bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
 	textdomain(GETTEXT_PACKAGE);
+	setlocale(LC_ALL,"");
 #endif
 
 	gui_error_logging_init("gui-install");
+	gtk_init(&argc, &argv);
 	g_option_context_add_main_entries(
 		option_context,
 		option_entries,
