00a1b40530ff22b23311b1868ce5f33422b5d187
Alexander Pyhalov <apyhalov@gmail.com>
2018-10-17 18:24:31 +0300

    Avoid doing fast reboot after system install

--- usr/src/cmd/gui-install/src/callbacks.c
+++ usr/src/cmd/gui-install/src/callbacks.c
@@ -28,7 +28,6 @@
 
 #include <errno.h>
 #include <sys/wait.h>
-#include <libbe.h>
 
 #include <glib.h>
 #include <glib/gi18n.h>
@@ -730,8 +729,6 @@ on_rebootbutton_clicked(GtkButton *button,
 	gchar *command_output = NULL;
 	gchar *command_error = NULL;
 	gint status = 0;
-	be_node_list_t *be_nodes = NULL;
-	be_node_list_t *be_cur = NULL;
 
 	/*
 	 * Reboot the system.  We don't eject the media first because reboot is
@@ -740,25 +737,12 @@ on_rebootbutton_clicked(GtkButton *button,
 	g_message("Rebooting the system NOW!.....\n");
 	command_path = g_find_program_in_path(REBOOT);
 	if (command_path) {
-		/* Get Path to Newly activated BE */
-		if (be_list(NULL, &be_nodes) == BE_SUCCESS) {
-			/* Determine newly created BE and fast reboot to it */
-			for (be_cur = be_nodes; be_cur != NULL;
-			    be_cur = be_cur->be_next_node) {
-				if (be_cur->be_active_on_boot) {
-					execl(command_path, REBOOT, "-f",
-					    be_cur->be_root_ds, NULL);
-				}
-			}
-		}
-		/* Fast reboot not possible or failed perform normal reboot */
+		/* Perform normal reboot */
 		execl(command_path, REBOOT, NULL);
 
 		g_warning("Failed to exec %s: %s",
 		    command_path,
 		    g_strerror(errno));
-		if (be_nodes != NULL)
-			be_free_list(be_nodes);
 		g_free(command_path);
 	} else
 		g_warning("Can't find reboot command in PATH!\n");
